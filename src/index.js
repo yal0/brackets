9d36bdbcc7820c47a5817eb5009a5a84580f5770
yal0 <onpol@mail.ru>
2022-02-24 15:33:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brakets = bracketsConfig.map(cur => cur.join(''));
+  while (true) {
+    let input = str;
+    for (let i = 0; i < brakets.length; i++) {
+      str = str.replace(brakets[i], '');
+    }
+    if (input == str) break;
+  }
+  return !Boolean(str);
 }
